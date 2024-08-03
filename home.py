d71016e6da650c42fc275baac7b29848ea0c1da8
naren579 <147800440+naren579@users.noreply.github.com>
2024-08-03 10:55:30 +0530

    Create home.py

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Set the title of the Streamlit app
+st.title("Request for Proposal (RFP)")
+
+# Add some introductory text
+st.write("""
+Welcome to the Request for Proposal (RFP) page.
+
+Please select the appropriate page for your RFP needs:
+- **Hitachi Content Platform (HCP)**: Navigate to the "Hitachi Content Platform" page for HCP-related RFPs.
+- **NetApp**: Navigate to the "NetApp" page for NetApp-related RFPs.
+""")
